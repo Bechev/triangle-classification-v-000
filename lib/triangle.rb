78d2d393897c26b2628babaf9241c397099ebb03
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-17 19:11:54 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,21 @@
 class Triangle
-  # write code here
+  def initialize (side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    if side_1 == side_2
+      if side_2 == side_3
+        return :equilateral
+      else
+        return :isosceles
+      end
+    else
+      return :scalene
+    end
+  end
 end
 
 class TriangleError < StandardError
