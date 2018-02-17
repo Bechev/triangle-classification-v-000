76b57ab7c1b432f1a6a8da5884b770f7768df794
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-17 19:28:31 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,16 +6,20 @@ class Triangle
   end
 
   def kind
-    if @side_1 == 0 || @side_2 == 0 || @side_3 == 0
+    if @side_1 <= 0 || @side_2 <= 0 || @side_3<= 0 
       raise TriangleError
+    elsif 
+      @side_1 + @side_2 < @side_3 ||
+      @side_1 + @side_3 < @side_2 ||
+      @side_2 + @side_3 < @side_1
+      raise TriangleError
+    end
+    if @side_1 == @side_2 && @side_2 == @side_3
+      return :equilateral
+    elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
+      return :isosceles
     else
-      if @side_1 == @side_2 && @side_2 == @side_3
-        return :equilateral
-      elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
-        return :isosceles
-      else
-        return :scalene
-      end
+      return :scalene
     end
   end
 end
