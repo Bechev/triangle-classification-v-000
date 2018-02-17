06f2a149a1b951a714eab004f1e82586cef1fd39
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-17 19:29:02 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,8 +13,7 @@ class Triangle
       @side_1 + @side_3 < @side_2 ||
       @side_2 + @side_3 < @side_1
       raise TriangleError
-    end
-    if @side_1 == @side_2 && @side_2 == @side_3
+    elsif @side_1 == @side_2 && @side_2 == @side_3
       return :equilateral
     elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
       return :isosceles
